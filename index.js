a6c799611f70245711d30d524d3ff232c8b07f86
FadhilKholaf <fadhilgd123@gmail.com>
2023-11-29 07:45:05 +0700

    fucking new commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const uri = "mongodb://localhost:27017";
 const mongo_client = new MongoClient(uri);
@@ -15,4 +15,33 @@ async function get_users() {
   console.log(users);
 }
 
+async function add_user(name, age) {
+  const connectDB = await conenction;
+  const insert_user = await connectDB
+    .db("belajar_crud")
+    .collection("user")
+    .insertOne({ name, age });
+
+  return insert_user;
+}
+
+async function update_user(id, name, age) {
+  const connectDB = await conenction;
+  const update_user = await connectDB
+    .db("belajar_crud")
+    .collection("user")
+    .updateOne({ _id: new ObjectId(id) }, { name, age });
+
+  return update_user;
+}
+
 get_users();
+
+const names = ["Fadhil", "Pandhu", "Sulthan", "Beni", "Lenovo"];
+
+add_user(
+  names[Math.floor(Math.random() * names.length)],
+  Math.floor(Math.random() * 20)
+);
+
+update_user("65668452b464cd29c5b465d9", "Ahsan gaming", 17);
